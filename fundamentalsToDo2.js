c540497528938a36340769a456d8e3d58af9d797
Erick Vivas <88099754+erickvivas415@users.noreply.github.com>
2022-04-04 19:44:49 -0700

    Add files via upload

---
+++ fundamentalsToDo2.js
@@ -0,0 +1,22 @@
+// Countdown: Create a function that accepts a number as an input. Return a new array that counts down by one, from the number (as array’s ‘zeroth’ element) down to 0 (as the last element). How long is this array?
+
+function countdown(num) {
+    var arr = [];
+    var temp = num;
+
+    for(var x=0;x<=num;x++) {
+        arr[x] = temp;
+        temp--
+    }
+
+    return arr;
+}
+
+console.log(countdown(9))
+
+// First plus length: Given an array, return the sum of the first value in the array, plus the array’s length. What happens if the array’s first value is not a number, but a string (like "what?") or a boolean (like false).
+
+function firstPlusLength(arr) {
+
+
+}
