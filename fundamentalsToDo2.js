580b5e2054c0cf751d41d42f01ad56f2a9a88331
Erick Vivas <88099754+erickvivas415@users.noreply.github.com>
2022-04-04 19:59:44 -0700

    Add files via upload

--- fundamentalsToDo2.js
+++ fundamentalsToDo2.js
@@ -17,6 +17,14 @@ console.log(countdown(9))
 // First plus length: Given an array, return the sum of the first value in the array, plus the array’s length. What happens if the array’s first value is not a number, but a string (like "what?") or a boolean (like false).
 
 function firstPlusLength(arr) {
-
-
+    if(typeof arr[0]  === "number") {
+        return arr[0]+arr.length;
+    }
+    else {
+        return "First value of the array is not a number";
+    }
 }
+
+a=[true,1,2,3,4]
+
+console.log(firstPlusLength(a))
